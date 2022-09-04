3630c36dddc3de1efb4c1827b428e6e155ee2a4a
eunminCho <silvermn99@gmail.com>
2022-09-04 14:39:18 +0900

    fix: error solved

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -3,7 +3,6 @@ import axiosInstance from 'axios';
 const axios = axiosInstance.create({
   baseURL: 'https://memint-server.herokuapp.com/',
   // baseURL: 'http://localhost:5000',
-  origin: location.origin()
 });
 
 export default axios;
